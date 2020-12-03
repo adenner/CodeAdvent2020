b937fd7252f36b6d46b0583da2e8538c3b748839
Andrew Denner <denner@gmail.com>
2020-12-03 01:56:17 -0600

    day 3 part 1

---
+++ Day03/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace Day03
+{
+    class Program
+    {
+        private static readonly ILogger log = LoggerFactory.Create(builder => builder.AddConsole()).CreateLogger<Program>();
+        static void Main(string[] args)
+        {
+            var currentpos = 0;
+            var hits = 0;
+            foreach (var line in File.ReadAllLines("Data.txt").Select(x=>x.Trim()))
+            {
+                var current = line.ElementAt(currentpos);
+                if (line.ElementAt(currentpos) == '#')
+                    hits++;
+                currentpos += 3;
+                if (currentpos >= line.Length)
+                    currentpos -= line.Length;
+            }
+            log.LogInformation($" We hit {hits} trees");
+
+            
+        }
+    }
+}
