275941d0def39c178ce4631a615b2ba29cd25503
Andrew Denner <denner@gmail.com>
2020-12-05 04:27:29 -0600

    day5 part1

---
+++ Day05/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+
+namespace Day05
+{
+    class Program
+    {
+        private static readonly ILogger log = LoggerFactory.Create(builder => builder.AddConsole()).CreateLogger<Program>();
+        static void Main(string[] args)
+        {
+            var inputs = File.ReadAllLines("input.txt");
+            var seatset = inputs.Select(x => new Seat(x)).Select(x => x.Id);
+            log.LogInformation($"Part1: max seat is {seatset.Max()}");
+        }
+    }
+}
---
+++ Day05/Seat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day05
+{
+    public class Seat
+    {
+        public Seat(string s)
+        {
+            var row = s.Replace("B", "1")
+                .Replace("F", "0")
+                .Replace("R", "1")
+                .Replace("L", "0");
+            Id = Convert.ToInt32(row, 2);
+        }
+        public int Id { get; }
+    }
+}
